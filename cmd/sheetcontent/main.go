560f5215846139ab0861521074959a393edb1b54
stay-focused-dev <maxim@stay-focused.dev>
2025-12-03 16:21:06 +0300

    fix some hardcoded constants

--- cmd/sheetcontent/main.go
+++ cmd/sheetcontent/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -12,10 +14,25 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
+func usage() {
+	program := filepath.Base(os.Args[0])
+	fmt.Printf("Usage: ./%s -creds CREDS_FILE -sheet GSHEET_ID", program)
+	os.Exit(1)
+}
+
 func main() {
 	ctx := context.Background()
 
-	credBytes, err := os.ReadFile("/tmp/credentials.json")
+	credentialsFile := flag.String("creds", "", "file with JSON credentials to GDrive API")
+	sheetId := flag.String("sheet", "", "sheet id")
+
+	flag.Parse()
+
+	if *credentialsFile == "" || *sheetId == "" {
+		usage()
+	}
+
+	credBytes, err := os.ReadFile(*credentialsFile)
 	if err != nil {
 		log.Fatalf("Unable to read credentials: %v", err)
 	}
@@ -36,10 +53,9 @@ func main() {
 		log.Fatalf("Unable to create sheets service: %v", err)
 	}
 
-	sheetId := "1W0wgyCA2MIasx5zsFCKODJk2iSUKx3APpzoDs-9mWXE"
 	readRange := "Лист1!A1:F10"
 
-	resp, err := sheetsService.Spreadsheets.Values.Get(sheetId, readRange).Do()
+	resp, err := sheetsService.Spreadsheets.Values.Get(*sheetId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data: %v", err)
 	}
